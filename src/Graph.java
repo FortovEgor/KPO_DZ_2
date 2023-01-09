dadebb8e469b204e272f874c0f7d4cf0570ad45a
Egor Fortov <egorfortov@gmail.com>
2023-01-09 20:09:30 +0300

    fixed all warnings in Graph class

--- src/Graph.java
+++ src/Graph.java
@@ -6,8 +6,8 @@ import java.util.*;
 // This class represents a directed graph
 // using adjacency_matrixacency list representation
 class Graph {
-    private int number_of_vertices;  // число вершин в графе
-    private ArrayList<ArrayList<Integer> > adjacency_matrix;  // УДОБНАЯ матрица смежности
+    private final int number_of_vertices;  // число вершин в графе
+    private final ArrayList<ArrayList<Integer>> adjacency_matrix;  // УДОБНАЯ матрица смежности
 
     Graph(int vertices_count) {
         number_of_vertices = vertices_count;
@@ -33,7 +33,7 @@ class Graph {
      * @return - true/false
      */
     public boolean isCyclic() {
-        int inDegree[] = new int[number_of_vertices];
+        int[] inDegree = new int[number_of_vertices];
         for (int i = 0; i < number_of_vertices; i++) {
             for (Integer it : adjacency_matrix.get(i)) {
                 inDegree[it]++;
@@ -57,10 +57,7 @@ class Graph {
                 }
             }
         }
-        if (cnt == number_of_vertices) {
-            return false;
-        }
-        return true;
+        return !(cnt == number_of_vertices);
     }
 
     /**
@@ -69,20 +66,19 @@ class Graph {
      * @param visited - массив посещенных вершин
      * @param stack - стек значений вершин, нужен для рекурсии
      */
-    void topologicalSortUtil(int v, boolean visited[], Stack<Integer> stack) {
+    void topologicalSortUtil(int v, boolean[] visited, Stack<Integer> stack) {
         visited[v] = true;  // теперь текущая вершина считается посещенной
         Integer i;
 
         // рекурсивно итерируемся по всем вершинам, к которым есть путь из v
-        Iterator<Integer> it = adjacency_matrix.get(v).iterator();
-        while (it.hasNext()) {
-            i = it.next();
+        for (Integer integer : adjacency_matrix.get(v)) {
+            i = integer;
             if (!visited[i])
                 topologicalSortUtil(i, visited, stack);
         }
 
         // добавляем текущую вершину в стек, который хранит результат
-        stack.push(Integer.valueOf(v));
+        stack.push(v);
     }
 
     /**
@@ -90,12 +86,12 @@ class Graph {
      * рекурсивную функцию выше
      * @param vec - вектор вершин
      */
-    void topologicalSort(Vector vec) {
+    void topologicalSort(Vector<Integer> vec) {
         Stack<Integer> stack = new Stack<Integer>();
 
         // Mark all the vertices as not visited
         // помечаем все вершины как неотмеченные
-        boolean visited[] = new boolean[number_of_vertices];
+        boolean[] visited = new boolean[number_of_vertices];
         for (int i = 0; i < number_of_vertices; i++)
             visited[i] = false;
 
@@ -104,41 +100,15 @@ class Graph {
         // Topological Sort starting
         // from all vertices one by one?
         for (int i = 0; i < number_of_vertices; i++)
-            if (visited[i] == false)
+            if (!visited[i])
                 topologicalSortUtil(i, visited, stack);
 
         // печатаем стек
-        while (stack.empty() == false) {
+        while (!stack.empty()) {
             int elem = stack.pop();
             vec.addElement(elem);
         }
         Collections.reverse(vec);
-    }
-
-    // Driver code
-    public static void main(String args[])
-    {
-//        // Create a graph given in the above diagram
-//        Graph g = new Graph(6);
-//        g.addEdge(5, 2);
-//        g.addEdge(4, 0);
-//        g.addEdge(4, 1);
-//        g.addEdge(2, 3);
-//        g.addEdge(3, 1);
-//
-////        g.printAdjMatrix();
-//        if (g.isCyclic()) {
-//            System.out.println("Cyclic");
-//        } else {
-//            System.out.println("NOT Cyclic");
-//        }
-//
-//
-//        System.out.println("Following is a Topological "
-//                + "sort of the given graph");
-//        // Function Call
-//        Vector vec = new Vector();
-//        g.topologicalSort(vec);
     }
 }
 // This code is contributed by Aakash Hasija
